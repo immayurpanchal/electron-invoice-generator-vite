115f9bb0ca636310be9f7b11cc71b869b1a493dd
Mayur Panchal <mayurpanchal002@gmail.com>
2023-03-19 12:34:37 +0530

    feat: :sparkles: add search select with db using antd

--- src/App.tsx
+++ src/App.tsx
@@ -3,58 +3,64 @@ import { useEffect, useState } from 'react';
 import Update from '@/components/update';
 import { Client } from 'pg';
 import './App.scss';
+import { Select } from 'antd';
 
 console.log('[App.tsx]', `Hello world from Electron ${process.versions.electron} ${process.versions.chrome}!`);
+
+const client = new Client({
+	user: 'test',
+	host: 'localhost',
+	database: 'test',
+	password: 'test',
+	port: 5432,
+});
+await client.connect();
 
 function App() {
 	const [count, setCount] = useState(0);
 	const [dbData, setDbData] = useState([]);
+	const [value, setValue] = useState('');
 
-	useEffect(() => {
-		const client = new Client({
-			user: 'test',
-			host: 'localhost',
-			database: 'test',
-			password: 'test',
-			port: 5432,
-		});
+	useEffect(() => {}, []);
 
-		const fetchData = async () => {
-			try {
-				await client.connect();
-				console.log('Connected to PostgreSQL database');
+	const fetchData = async (query: string) => {
+		try {
+			console.log('Connected to PostgreSQL database');
+			const res = await client.query(`SELECT * FROM products WHERE product_name ILIKE '%${query}%';`);
+			const ans = res.rows.map((item) => ({ label: item?.product_name, value: item?.id }));
+			setDbData(ans);
+		} catch (err) {
+			console.error('Error querying PostgreSQL database:', err);
+		} finally {
+			console.log('return called');
+			// await client.end();
+		}
+	};
 
-				const res = await client.query('SELECT * FROM products');
-				setDbData(res.rows);
-			} catch (err) {
-				console.error('Error querying PostgreSQL database:', err);
-			} finally {
-				console.log('return called');
-				await client.end();
-			}
-		};
+	const onSearch = (value: string) => {
+		if (value.length < 3) return;
+		fetchData(value);
+	};
 
-		fetchData();
-
-		return () => {
-			console.log('Cleanup function called');
-		};
-	}, []);
+	const onChange = (newValue: string) => {
+		setValue(newValue);
+	};
 
 	return (
 		<div className='App'>
 			<div>
-				<h1>PostgreSQL Database Data</h1>
-				<ul>
-					{dbData.map((row) => (
-						<li key={row.id}>{row.product_name}</li>
-					))}
-				</ul>
-			</div>
-			<div>
-				<a href='https://github.com/electron-vite/electron-vite-react' target='_blank'>
-					<img src='./electron-vite.svg' className='logo' alt='Electron + Vite logo' />
-				</a>
+				<Select
+					value={value}
+					placeholder='Hello world'
+					notFoundContent={null}
+					filterOption={false}
+					onChange={onChange}
+					defaultActiveFirstOption={false}
+					showSearch
+					onSearch={onSearch}
+					options={dbData}
+					style={{ width: '50%' }}
+				/>
 			</div>
 			<h1>Electron + Vite + React + Mayur</h1>
 			<div className='card'>
