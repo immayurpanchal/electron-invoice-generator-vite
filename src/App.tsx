b62a45dbc4302521e941afb0ebe90a814c3e1033
Mayur Panchal <mayurpanchal002@gmail.com>
2023-03-31 19:40:23 +0530

    feat: :sparkles: implement product comp using ipc hook
    to fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.scss'
+import ProductList from './components/Product'
 import SalesInvoice from './components/SalesInvoice/SalesInvoice'
-
 function App() {
   return (
     <div className='App'>
+      <ProductList />
       <SalesInvoice />
     </div>
   )
