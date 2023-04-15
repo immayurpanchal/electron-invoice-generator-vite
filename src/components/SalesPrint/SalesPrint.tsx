0baf148b8a5b120048ba2625a36e97abcfcbcef0
Mayur Panchal <mayurpanchal002@gmail.com>
2023-04-15 17:52:32 +0530

    refactor(fe): :recycle: SalesPrint to get data from context
    remove mock data

--- src/components/SalesPrint/SalesPrint.tsx
+++ src/components/SalesPrint/SalesPrint.tsx
@@ -1,133 +1,11 @@
+import { BillContext } from '@/App'
 import { Col, Row, Table } from 'antd'
 import { ColumnsType } from 'antd/es/table'
-import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useContext, useEffect } from 'react'
+import { BillTableProduct } from '../SalesInvoice/SalesInvoice'
 import './SalesPrint.scss'
 
-const mockData = [
-  {
-    product_name:
-      'MSV CHAIN 001 80 POINT BLOUSE PIECE IN 2 CUTS AND  PIECE AND LONGER NAME WITH BEAUTIFUL CUTS',
-    product_price: 49.99,
-    qty: 2,
-    id: 1,
-    bill_price: 148599.98,
-  },
-  {
-    product_name: 'LADY HEROINE CUT SIZE BLOUSE PIECE FULL NECK HALF NECK',
-    product_price: 29.99,
-    qty: 3,
-    id: 2,
-    bill_price: 8549.97,
-  },
-  {
-    product_name: 'Shoes',
-    product_price: 89.99,
-    qty: 1,
-    id: 3,
-    bill_price: 859.99,
-  },
-  {
-    product_name: 'Jeans',
-    product_price: 59.99,
-    qty: 2,
-    id: 4,
-    bill_price: 119.98,
-  },
-  {
-    product_name: 'T-shirt',
-    product_price: 19.99,
-    qty: 4,
-    id: 5,
-    bill_price: 79.96,
-  },
-  // Add remaining 15 items here
-  {
-    product_name: 'Item6',
-    product_price: 39.99,
-    qty: 3,
-    id: 6,
-    bill_price: 119.97,
-  },
-  {
-    product_name: 'Item7',
-    product_price: 69.99,
-    qty: 1,
-    id: 7,
-    bill_price: 69.99,
-  },
-  {
-    product_name: 'Item8',
-    product_price: 14.99,
-    qty: 5,
-    id: 8,
-    bill_price: 74.95,
-  },
-  {
-    product_name: 'Item9',
-    product_price: 99.99,
-    qty: 2,
-    id: 9,
-    bill_price: 199.98,
-  },
-  {
-    product_name: 'Item10',
-    product_price: 49.99,
-    qty: 3,
-    id: 10,
-    bill_price: 149.97,
-  },
-  {
-    product_name: 'Item11',
-    product_price: 79.99,
-    qty: 1,
-    id: 11,
-    bill_price: 79.99,
-  },
-  {
-    product_name: 'Item12',
-    product_price: 24.99,
-    qty: 4,
-    id: 12,
-    bill_price: 99.96,
-  },
-  {
-    product_name: 'Item13',
-    product_price: 54.99,
-    qty: 2,
-    id: 13,
-    bill_price: 109.98,
-  },
-  {
-    product_name: 'Item14',
-    product_price: 29.99,
-    qty: 3,
-    id: 14,
-    bill_price: 89.97,
-  },
-  {
-    product_name: 'Item15',
-    product_price: 39.99,
-    qty: 1,
-    id: 15,
-    bill_price: 39.99,
-  },
-  {
-    product_name: 'Item16',
-    product_price: 19.99,
-    qty: 5,
-    id: 16,
-    bill_price: 99.95,
-  },
-  {
-    product_name: 'Item17',
-    product_price: 64.99,
-    qty: 2,
-    id: 17,
-    bill_price: 129.98,
-  },
-]
-const columns: ColumnsType<any> = [
+const columns: ColumnsType<BillTableProduct> = [
   {
     title: 'Sr.No.',
     render: (_value, _record, index) => index + 1,
@@ -146,7 +24,12 @@ const columns: ColumnsType<any> = [
     width: 50,
   },
   {
-    title: 'Amount',
+    title: 'Price',
+    dataIndex: 'product_price',
+    align: 'right',
+  },
+  {
+    title: 'Net Amount',
     dataIndex: 'bill_price',
     align: 'right',
     width: 100,
@@ -154,7 +37,7 @@ const columns: ColumnsType<any> = [
 ]
 
 const SalesPrint = () => {
-  const navigate = useNavigate()
+  const { billValue } = useContext(BillContext)
 
   useEffect(() => {
     const handlePrint = (event: KeyboardEvent) => {
@@ -182,8 +65,8 @@ const SalesPrint = () => {
   }, [])
 
   return (
-    <div className='a4-box p-3'>
-      <Col className='border-black border rounded-xl p-5 flex flex-col gap-y-4'>
+    <div className='p-3 a4-box'>
+      <Col className='flex flex-col p-5 border border-black rounded-xl gap-y-4'>
         <Row justify='space-between'>
           <span>Debit Memo</span>
           <span>Subject to Idar Jurisdiction</span>
@@ -230,7 +113,7 @@ const SalesPrint = () => {
         <Row>
           <Col span={24}>
             <Table
-              dataSource={mockData}
+              dataSource={billValue}
               columns={columns}
               pagination={false}
               size='small'
