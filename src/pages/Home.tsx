398f7b75505eb62174af439a6a4970c0e60feff0
Mayur Panchal <mayurpanchal002@gmail.com>
2023-04-15 11:51:52 +0530

    feat(fe): :sparkles: implement & integrate delete all products

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,11 @@
-import { Menu } from 'antd'
-import { Outlet, useNavigate } from 'react-router-dom'
+import { useIpcMutate } from '@/hooks/useIpcMutate'
+import { Menu, notification } from 'antd'
+import { ItemType } from 'antd/es/menu/hooks/useItems'
 import type { MenuClickEventHandler } from 'node_modules/rc-menu/lib/interface'
+import { useEffect } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
 
-const menuItems = [
+const menuItems: ItemType[] = [
   {
     label: 'File',
     key: 'file',
@@ -18,12 +21,65 @@ const menuItems = [
       },
     ],
   },
+  {
+    label: 'Tools',
+    key: 'tools',
+    children: [
+      {
+        label: 'Delete All Products',
+        key: 'delete_all_products',
+      },
+      {
+        label: 'Delete All Customers',
+        key: 'delete_all_customers',
+      },
+      {
+        label: 'Reset Entire App',
+        key: 'delete_all_tables',
+      },
+    ],
+  },
 ]
 
 const Home = () => {
   const navigate = useNavigate()
+  const {
+    mutate: deleteAllProducts,
+    data,
+    error,
+  } = useIpcMutate('deleteAllProducts')
+
+  useEffect(() => {
+    if (data) {
+      notification.success({
+        message: 'Deleted All Products successfully',
+        placement: 'top',
+      })
+    }
+  }, [data])
+
+  useEffect(() => {
+    if (error) {
+      notification.error({
+        message: 'Failed to Deleted All Products!',
+        description: error.message,
+        placement: 'top',
+      })
+    }
+  }, [error])
+
+  const handleToolsMenu = async (key: string) => {
+    if (key === 'delete_all_products') {
+      await deleteAllProducts()
+    }
+  }
+
   const handleClick: MenuClickEventHandler = (e) => {
-    navigate(`/${e.key}`)
+    if (e.keyPath.includes('tools')) {
+      return handleToolsMenu(e.key)
+    } else if (e.keyPath.includes('file')) {
+      navigate(`/${e.key}`)
+    }
   }
 
   return (
