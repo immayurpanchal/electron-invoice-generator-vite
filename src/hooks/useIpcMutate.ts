b0f505665800e358ec662e322eb186f76cb20eae
Mayur Panchal <mayurpanchal002@gmail.com>
2023-04-15 11:07:28 +0530

    feat(fe): :sparkles: create useIpcMutate generic hook

---
+++ src/hooks/useIpcMutate.ts
@@ -0,0 +1,17 @@
+import { ipcRenderer } from 'electron'
+import { useState } from 'react'
+
+export const useIpcMutate = (channel: string) => {
+  const [data, setData] = useState(null)
+  const [error, setError] = useState<Error | null>(null)
+  const mutate = async (data: any) => {
+    try {
+      const response = await ipcRenderer.invoke(channel, data)
+      setData(response)
+    } catch (err: any) {
+      setError(err)
+    }
+  }
+
+  return { mutate, data, isLoading: !!data, error }
+}
