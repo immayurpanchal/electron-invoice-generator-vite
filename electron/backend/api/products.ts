38edbc3d672573aabf7dfd5abc3a49ce6b5c6869
Mayur Panchal <mayurpanchal002@gmail.com>
2023-04-15 11:07:01 +0530

    feat(be): :sparkles: integrate addProduct api

--- electron/backend/api/products.ts
+++ electron/backend/api/products.ts
@@ -1,11 +1,26 @@
 import { ipcMain } from 'electron'
 import { prisma } from './shared/prismaClient'
 
-ipcMain.handle('getProducts', async (event, args) => {
+ipcMain.handle('getProducts', async () => {
   try {
     const products = await prisma.products.findMany()
     return products
   } catch (err) {
     console.error(err)
+  }
+})
+
+ipcMain.handle('addProduct', async (_event, args) => {
+  try {
+    const product = await prisma.products.create({
+      data: {
+        product_name: args.productName,
+        qty: args.productQty,
+      },
+    })
+    return product
+  } catch (err) {
+    console.error(err)
+    return err
   }
 })
